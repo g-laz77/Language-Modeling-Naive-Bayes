ba7ecc53cc1b2bd4abb647ee9860960344ccb03b
g-laz77 <suhanprabhu@gmail.com>
2017-09-13 20:18:32 +0530

    found all n-gram probabilities

--- tokenizer.py
+++ tokenizer.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import operator
 import numpy as np
 import math
-
+unigrams_dict = dict()
+bigrams_dict = dict()
+trigrams_dict = dict()
 file1 = sys.argv[1]
 scanner=re.Scanner([
   (r"[0-9]+",       lambda scanner,token:("INTEGER", token)),
@@ -26,7 +28,9 @@ def tokenize(filename):
 def language_modeling(tokens):
     unigrams = list()                #unigram model
     unigrams = tokens
+    unigrams_dict = vocabulary
 
+    bigrams_dict = dict()
     bigrams = list()
     for i in range(len(tokens)-1):   #bigram models
         temp = list()
@@ -34,6 +38,7 @@ def language_modeling(tokens):
         temp.append(tokens[i+1])
         bigrams.append(temp)
     # print(bigrams)
+    
 
     trigrams = list()
     for i in range(len(tokens)-2):   #trigram models
@@ -43,19 +48,53 @@ def language_modeling(tokens):
         temp.append(tokens[i+2])
         trigrams.append(temp)
 
+    return bigrams,trigrams
 
 list_tokens = tokenize(file1)               #making the vocabulary
+bigrams,trigrams = language_modeling(list_tokens)
 vocabulary = dict()
 for i in range(len(list_tokens)):
     if vocabulary.get(list_tokens[i]) == None:
         vocabulary[list_tokens[i]] = 1
     else:
         vocabulary[list_tokens[i]] += 1
+vocab_length = len(vocabulary)
 
+unigram_probs = dict()
+for i in range(len(list_tokens)):
+    if unigram_probs.get(list_tokens[i]) == None:
+        unigram_probs[list_tokens[i]] = vocabulary[list_tokens[i]]/len(list_tokens)
+
+for i in range(len(bigrams)-1):
+    temp = bigrams[i][0] + '_' + bigrams[i][1]
+    if bigrams_dict.get(temp) == None:
+        bigrams_dict[temp] = 1
+    else:
+        bigrams_dict[temp] += 1
+bg_vocab_len = len(bigrams_dict)
+
+bigram_probs = dict()
+for i in range(len(bigrams)-1):
+    temp = bigrams[i][0] + '_' + bigrams[i][1]
+    if bigrams_probs.get(temp) == None:
+        bigrams_probs[temp] = bigrams_dict[temp]/vocabulary[bigrams[i][0]]
+
+for i in range(len(trigrams)-1):
+    temp = trigrams[i][0] + '_' + trigrams[i][1] + '_' + trigrams[i][2]
+    if trigrams_dict.get(temp) == None:
+        trigrams_dict[temp] = 1
+    else:
+        triigrams_dict[temp] += 1
+trg_vocab_len = len(trigrams_dict)
+
+trigram_probs =  dict()
+for i in range(len(trigrams)-1):
+    temp = trigrams[i][0] + '_' + trigrams[i][1] + '_' + trigrams[i][2]
+    if trigrams_probs.get(temp) == None:
+        trigrams_probs[temp] = trigrams_dict[temp]/bigrams_dict[trigrams[i][1]+'_'+trigrams[i][2]]
 
 def make_zipf():                        #plotting the zipf curve
     sort_freq = sorted(vocabulary.items(), key = operator.itemgetter(1),reverse=True)
-
     zipf_x = list()
     zipf_y = list()
     log_zipf_y = list()
@@ -74,9 +113,8 @@ def make_zipf():                        #plotting the zipf curve
     plt.plot(log_x_axis,log_zipf_y,label="logcurve")
     plt.show()
 
-#def laplace_smoothing():
-
-
 make_zipf()
+
+def laplace_smoothing():                    #laplace smoothing
 
 
