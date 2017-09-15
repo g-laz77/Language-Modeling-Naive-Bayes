1f55c2eb427ea6353b21ae2ff7640cdde77b12a1
g-laz77 <suhanprabhu@gmail.com>
2017-09-15 16:58:13 +0530

    implemented witten bell

--- tokenizer.py
+++ tokenizer.py
@@ -65,7 +65,7 @@ for i in range(len(list_tokens)):
     if unigram_probs.get(list_tokens[i]) == None:
         unigram_probs[list_tokens[i]] = vocabulary[list_tokens[i]]/len(list_tokens)
 
-for i in range(len(bigrams)-1):
+for i in range(len(bigrams)):
     temp = bigrams[i][0] + '_' + bigrams[i][1]
     if bigrams_dict.get(temp) == None:
         bigrams_dict[temp] = 1
@@ -74,12 +74,12 @@ for i in range(len(bigrams)-1):
 bg_vocab_len = len(bigrams_dict)
 
 bigrams_probs = dict()
-for i in range(len(bigrams)-1):
+for i in range(len(bigrams)):
     temp = bigrams[i][0] + '_' + bigrams[i][1]
     if bigrams_probs.get(temp) == None:
         bigrams_probs[temp] = bigrams_dict[temp]/vocabulary[bigrams[i][0]]
 
-for i in range(len(trigrams)-1):
+for i in range(len(trigrams)):
     temp = trigrams[i][0] + '_' + trigrams[i][1] + '_' + trigrams[i][2]
     if trigrams_dict.get(temp) == None:
         trigrams_dict[temp] = 1
@@ -88,7 +88,7 @@ for i in range(len(trigrams)-1):
 trg_vocab_len = len(trigrams_dict)
 
 trigrams_probs =  dict()
-for i in range(len(trigrams)-1):
+for i in range(len(trigrams)):
     temp = trigrams[i][0] + '_' + trigrams[i][1] + '_' + trigrams[i][2]
     if trigrams_probs.get(temp) == None:
         trigrams_probs[temp] = trigrams_dict[temp]/bigrams_dict[trigrams[i][1]+'_'+trigrams[i][2]]
@@ -113,27 +113,23 @@ def make_zipf():                        #plotting the zipf curve
     plt.plot(log_x_axis,log_zipf_y,label="logcurve")
     plt.show()
 
-make_zipf()
+#make_zipf()
 
 def laplace_smoothing():                    #laplace smoothing        
     for i in range(len(list_tokens)):
         if ll_unigrams_probs.get(list_tokens[i]) == None:
             ll_unigrams_probs[list_tokens[i]] = (vocabulary[list_tokens[i]]+1)/(len(list_tokens)+vocab_length)
 
-    for i in range(len(bigrams)-1):
+    for i in range(len(bigrams)):
         temp = bigrams[i][0] + '_' + bigrams[i][1]
         if ll_bigrams_probs.get(temp) == None:
             ll_bigrams_probs[temp] = (bigrams_dict[temp]+1)/(vocabulary[bigrams[i][0]]+vocab_length)
 
-    for i in range(len(trigrams)-1):
+    for i in range(len(trigrams)):
         temp = trigrams[i][0] + '_' + trigrams[i][1] + '_' + trigrams[i][2]
         if ll_trigrams_probs.get(temp) == None:
             ll_trigrams_probs[temp] = (trigrams_dict[temp]+1)/(bigrams_dict[trigrams[i][1]+'_'+trigrams[i][2]]+ len(list_tokens)-1)
 
-# ll_unigrams_probs = dict()
-# ll_bigrams_probs = dict()
-# ll_trigrams_probs =  dict()
-# laplace_smoothing()
 
 def smoothing_curve(uni_probs,bi_probs,tri_probs):
     uni_sort_freq = sorted(uni_probs.items(), key = operator.itemgetter(1),reverse=True)
@@ -169,7 +165,11 @@ def smoothing_curve(uni_probs,bi_probs,tri_probs):
     plt.plot(curve_x,curve_y)
     plt.show()
 
-#smoothing_curve(ll_unigrams_probs,ll_bigrams_probs,ll_trigrams_probs)
+# ll_unigrams_probs = dict()
+# ll_bigrams_probs = dict()
+# ll_trigrams_probs =  dict()
+# laplace_smoothing()
+# smoothing_curve(ll_unigrams_probs,ll_bigrams_probs,ll_trigrams_probs)
 
 def wb_prob(word1,word2,word3,level):
     if level == 3:
@@ -183,7 +183,6 @@ def wb_prob(word1,word2,word3,level):
         if wb_bigrams_probs.get(word1+"_"+word2) == None:
            wb_bigrams_probs[word1+"_"+word2] = wb_prob(word1,word2,"",2)   
         return ((lamb*trigrams_probs[word1+"_"+word2+"_"+word3]) + (onelambda)*wb_bigrams_probs[word1+"_"+word2])
-
     elif level == 2:
         count = 0
         for key in bigrams_dict.keys():
@@ -196,19 +195,19 @@ def wb_prob(word1,word2,word3,level):
 
 
 def witten_bell():
-    for i in range(len(trigrams)-1):
-        temp = trigrams[i][0] + '_' + trigrams[i][1] + '_' + trigrams[i][2]
+    for i in range(len(trigrams)):
+        temp = trigrams[i][0] + '_' + trigrams[i][1] +'_' + trigrams[i][2]
         if wb_trigrams_probs.get(temp) == None:
             wb_trigrams_probs[temp] = wb_prob(trigrams[i][0],trigrams[i][1],trigrams[i][2],3)
     
-    for i in range(len(bigrams)-1):
-        temp = bigrams[i][0] + '_' + bigrams[i][1] + '_' + bigrams[i][2]
+    for i in range(len(bigrams)):
+        temp = bigrams[i][0] + '_' + bigrams[i][1]
         if wb_bigrams_probs.get(temp) == None:
             wb_bigrams_probs[temp] = wb_prob(bigrams[i][0],bigrams[i][1],"",2)
     
     for i in range(len(list_tokens)):
         if wb_unigrams_probs.get(list_tokens[i]) == None:
-            wb_unigrams_probs[list_tokens[i]] = unigram_probs[list_token[i]]
+            wb_unigrams_probs[list_tokens[i]] = unigram_probs[list_tokens[i]]
 
 wb_unigrams_probs = dict()
 wb_bigrams_probs = dict()
